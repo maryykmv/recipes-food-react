dd2ec6d72577367b35b49bfb0d522750702aa6b7
Mary K <wildcat.3333@yandex.ru>
2023-06-05 23:49:58 +0700

    Add models app users and api

--- backend/foodgram/api/models.py
+++ backend/foodgram/api/models.py
@@ -1,77 +1,184 @@
-# from django.contrib.auth import get_user_model
+from django.contrib.auth import get_user_model
 from django.db import models
 from django.utils.translation import gettext_lazy as _
-from users.models import User
-# User = get_user_model()
+
+User = get_user_model()
 
 
-class Ingredient(models.Model):
-    name = models.CharField(max_length=200)
-    measurement_unit = models.CharField(max_length=10)
+class Tag(models.Model):
+    name = models.CharField(
+        verbose_name=_('Название'),
+        help_text=_('Название'),
+        max_length=200,
+        unique=True
+    )
+    color = models.CharField(
+        verbose_name=_('Цветовой HEX-код'),
+        help_text=_('Цветовой HEX-код'),
+        max_length=7,
+        unique=True,
+        default='#49B64E'
+    )
+    slug = models.SlugField(
+        verbose_name=_('Идентификатор'),
+        help_text=_('Идентификатор'),
+        max_length=200,
+        unique=True
+    )
+
+    class Meta:
+        verbose_name = 'Тег'
+        verbose_name_plural = 'Теги'
 
     def __str__(self):
         return self.name
 
 
-class Tag(models.Model):
-    name = models.CharField(max_length=16, unique=True)
-    color = models.CharField(max_length=16, unique=True)
-    slug = models.SlugField(unique=True)
+class Ingredient(models.Model):
+    name = models.CharField(
+        verbose_name=_('Название'),
+        help_text=_('Название'),
+        max_length=200
+    )
+    measurement_unit = models.CharField(
+        verbose_name=_('Единица измерения'),
+        help_text=_('Единица измерения'),
+        max_length=200
+    )
+
+    class Meta:
+        verbose_name = 'Ингредиент'
+        verbose_name_plural = 'Ингредиенты'
+
+    def __str__(self):
+        return self.name
 
 
 class Recipe(models.Model):
     author = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name='foods'
+        User,
+        on_delete=models.CASCADE,
+        related_name='recipes',
+        verbose_name='Автор'
     )
-    name = models.CharField(max_length=200)
+    name = models.CharField(
+        'Название',
+        max_length=200
+    )
     image = models.ImageField(
-        upload_to='foods/'
+        verbose_name=_('Изображение'),
+        help_text=_('Изображение'),
+        upload_to='recipes/'
     )
-    text = models.TextField()
-    ingredients = models.ManyToManyField(Ingredient)
-    tags = models.ManyToManyField(Tag)
-    cooking_time = models.IntegerField()
-
-    def __str__(self):
-        return (f'id: {self.id}, name: {self.text[:15]}'
-                f', author: {self.author.username}'
-                )
-
-
-class ShoppingList(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    recipes = models.ForeignKey(Recipe, on_delete=models.CASCADE)
-
-
-class Favorite(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    recipes = models.ForeignKey(Recipe, on_delete=models.CASCADE)
-
-
-class Subscription(models.Model):
-    user = models.ForeignKey(
-        User,
-        on_delete=models.CASCADE,
-        related_name='subscriber',
-        verbose_name=_('Подписчик'),
-        help_text=_('Пользователь, который подписывается')
+    text = models.TextField(
+        verbose_name=_('Описание'),
+        help_text=_('Описание')
     )
-    author = models.ForeignKey(
-        User,
-        on_delete=models.CASCADE,
-        related_name='author',
-        verbose_name=_('Автор'),
-        help_text=_('Автор рецепта')
+    ingredients = models.ManyToManyField(
+        Ingredient,
+        through='RecipeIngredient',
+        verbose_name=_('Ингредиенты'),
+        help_text=_('Ингредиенты')
+
+    )
+    tags = models.ManyToManyField(
+        Tag,
+        verbose_name=_('Теги'),
+        help_text=_('Теги')
+    )
+    cooking_time = models.IntegerField(
+        verbose_name=_('Время приготовления в минутах'),
+        help_text=_('Время приготовления в минутах')
     )
 
     class Meta:
+        ordering = ['-id']
+        verbose_name = 'Рецепт'
+        verbose_name_plural = 'Рецепты'
         constraints = [
-            models.UniqueConstraint(
-                fields=['user', 'author'],
-                name='unique author')
+            models.UniqueConstraint(fields=['author', 'name'],
+                                    name='unique_author_name')
         ]
-        verbose_name = 'Подписка'
-        verbose_name_plural = 'Подсписки'
 
     def __str__(self):
-        return f'{self.author.username}, {self.user.username}'
+        return self.name
+
+
+class RecipeIngredient(models.Model):
+    recipe = models.ForeignKey(
+        Recipe,
+        on_delete=models.CASCADE,
+        related_name='recipes',
+        verbose_name='Рецепт'
+    )
+    ingredient = models.ForeignKey(
+        Ingredient,
+        on_delete=models.CASCADE,
+        related_name='recipes',
+        verbose_name='Ингредиент'
+    )
+    count = models.IntegerField(
+        verbose_name=_('Количество'),
+        help_text=_('Количество')
+    )
+
+    class Meta:
+        ordering = ['-id']
+        verbose_name = 'Количество ингредиента'
+        verbose_name_plural = 'Количество ингредиентов'
+        constraints = [
+            models.UniqueConstraint(fields=['recipe', 'ingredient'],
+                                    name='unique_recipe_ingredient')
+        ]
+
+
+class Favorite(models.Model):
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='favorites',
+        verbose_name=_('Автор')
+    )
+    recipe = models.ForeignKey(
+        Recipe,
+        on_delete=models.CASCADE,
+        related_name='favorites',
+        verbose_name=_('Список избранного')
+    )
+
+    class Meta:
+        verbose_name = 'Избранное'
+        verbose_name_plural = 'Избранные'
+        constraints = [
+            models.UniqueConstraint(fields=['user', 'recipe'],
+                                    name='unique_favorite_recipe')
+        ]
+
+    def __str__(self):
+        return f'{self.recipe} {self.user}'
+
+
+class ShoppingList(models.Model):
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='list',
+        verbose_name=_('Автор')
+    )
+    recipe = models.ForeignKey(
+        Recipe,
+        on_delete=models.CASCADE,
+        related_name='list',
+        verbose_name=_('Список покупок')
+    )
+
+    class Meta:
+        verbose_name = 'Список покупок'
+        verbose_name_plural = 'Списки покупок'
+        constraints = [
+            models.UniqueConstraint(fields=['user', 'recipe'],
+                                    name='unique_user_recipe')
+        ]
+
+    def __str__(self):
+        return f'{self.recipe} {self.user}'
--- backend/foodgram/users/models.py
+++ backend/foodgram/users/models.py
@@ -1,47 +1,62 @@
-from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.db import models
 from django.utils.translation import gettext_lazy as _
-from django.contrib.auth.base_user import BaseUserManager
 
 
-class UserManager(BaseUserManager):
-    use_in_migrations = True
-
-    def _create_user(self, email, password, **extra_fields):
-        if not email:
-            raise ValueError('Users require an email field')
-        email = self.normalize_email(email)
-        user = self.model(email=email, **extra_fields)
-        user.set_password(password)
-        user.save(using=self._db)
-        return user
-
-    def create_user(self, email, password=None, **extra_fields):
-        extra_fields.setdefault('is_staff', False)
-        extra_fields.setdefault('is_superuser', False)
-        return self._create_user(email, password, **extra_fields)
-
-    def create_superuser(self, email, password, **extra_fields):
-        extra_fields.setdefault('is_staff', True)
-        extra_fields.setdefault('is_superuser', True)
-
-        if extra_fields.get('is_staff') is not True:
-            raise ValueError('Superuser must have is_staff=True.')
-        if extra_fields.get('is_superuser') is not True:
-            raise ValueError('Superuser must have is_superuser=True.')
-
-        return self._create_user(email, password, **extra_fields)
-
-
-class User(AbstractUser):
-    email = models.EmailField(_('email address'), unique=True)
-
-    objects = UserManager()
+class CustomUser(AbstractUser):
+    email = models.EmailField(
+        verbose_name=_('email'),
+        help_text=_('email'),
+        max_length=100,
+        unique=True
+    )
+    first_name = models.CharField(
+        verbose_name=_('Имя'),
+        help_text=_('Имя'),
+        max_length=30,
+        blank=False,
+        null=False
+    )
+    last_name = models.CharField(
+        verbose_name=_('Фамилия'),
+        help_text=_('Фамилия'),
+        max_length=150,
+        blank=False,
+        null=False)
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['username', 'first_name', 'last_name' #, 'is_subscribed'
-                       ]
+    REQUIRED_FIELDS = [
+        'username',
+        'password',
+        'first_name',
+        'last_name',
+    ]
+
+
+class Subscription(models.Model):
+    user = models.ForeignKey(
+        CustomUser,
+        on_delete=models.CASCADE,
+        related_name='follower',
+        verbose_name=_('Подписчик'),
+        help_text=_('Пользователь, который подписывается')
+    )
+    author = models.ForeignKey(
+        CustomUser,
+        on_delete=models.CASCADE,
+        related_name='following',
+        verbose_name=_('Автор'),
+        help_text=_('Автор рецептов')
+    )
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=['user', 'author'],
+                name='unique_following')
+        ]
+        verbose_name = 'Подписка'
+        verbose_name_plural = 'Подсписки'
 
     def __str__(self):
-        """ Return string representation of our user """
-        return self.email
+        return f'{self.user} {self.author}'
