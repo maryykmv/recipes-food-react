5fe3b22a03b73cb0cba11232a08c2ef685953e2d
Mary K <wildcat.3333@yandex.ru>
2023-06-10 19:28:23 +0700

    add pagination

--- backend/api/pagination.py
+++ backend/api/pagination.py
@@ -4,3 +4,4 @@ from rest_framework.pagination import PageNumberPagination
 
 class CustomPagination(PageNumberPagination):
     page_size = settings.PAGE_SIZE
+    page_size_query_param = 'page_size'
